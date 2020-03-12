d1652eaca066c759bc9db6842f39f0853f48d671
Maarten van 't Wout <m.a.vantwout@student.tudelft.nl>
2020-03-12 19:09:14 +0100

    minizinc add

--- MinizincRunner/src/Main.java
+++ MinizincRunner/src/Main.java
@@ -7,83 +7,82 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        MiniZincRunner runMZ = new MiniZincRunner();
-//        runMZ.looper();
-        File dir = new File("C:\\Users\\Maarten\\Desktop\\IDM\\gametheory_project\\TestSetGenerator\\TestSets\\RealisticSets");
-        File[] directoryListing = dir.listFiles();
-//        for (int i = 0; i < directoryListing.length; i++)
-        for (int problem = 35; problem < 36; problem++) {
-
-            File file = directoryListing[problem];
-            DataReader reader = new DataReader(file);
-            int numJobs = reader.numJobs;
-            int numAgents = reader.numAgents;
-            int[] processingTimes = reader.processingTimes;
-            //preferences = int[numAgents][numJobs];
-            int[][] preferences = reader.preferences;
-            int[][] preferencesInverse = new int[numAgents][numJobs];
-
-            for (int a = 0; a < numAgents; a++) {
-                for (int j = 0; j < numJobs; j++) {
-                    preferencesInverse[a][preferences[a][j] - 1] = j + 1;
-                }
-            }
+        MiniZincRunner runMZ = new MiniZincRunner();
+        runMZ.looper();
+//        File dir = new File("C:\\Users\\Maarten\\Desktop\\IDM\\gametheory_project\\TestSetGenerator\\TestSets\\RealisticSets");
+//        File[] directoryListing = dir.listFiles();
+//        for (int problem = 35; problem < 36; problem++) {
 //
-            System.out.println(numJobs);
-            System.out.println(numAgents);
-            System.out.println(Arrays.toString(processingTimes));
-            System.out.println(Arrays.toString(preferences[0]));
-            for (int y = 1; y < numAgents; y++) {
-                System.out.println(Arrays.toString(preferences[y]));
-            }
-
-            //score Array
-            int[] jobScore = new int[numJobs];
-
-            for (int jobk = 0; jobk < numJobs; jobk++) {
-                for (int jobl = 0; jobl < numJobs; jobl++) {
-                    int count = 0;
-                    if (jobk != jobl) {
-                        for (int agent = 0; agent < numAgents; agent++) {
-                            if (preferencesInverse[agent][jobk] < preferencesInverse[agent][jobl]) {
-                                count++;
-                            }
-                        }
-                        if ((float) count >= (((float) processingTimes[jobk] / ((float) processingTimes[jobk] + (float) processingTimes[jobl])) * (float) numAgents)) {
-                            jobScore[jobk]++;
-                        }
-                    }
-                }
-            }
-
-            System.out.println("Jobscore: ");
-            for(int q = 0; q < jobScore.length; q++){
-                System.out.print(jobScore[q] + ", ");
-            }
-
-            // Job on position
-            int[] positionJob = new int[numJobs];
-
-            // Used Job
-            boolean[] jobUsed = new boolean[numJobs];
-
-            for (int i = 0; i < numJobs; i++) {
-                int score = -1;
-                int index = -1;
-                for (int j = 0; j < numJobs; j++) {
-                    if (score < jobScore[j] && jobUsed[j] == false) {
-                        score = jobScore[j];
-                        index = j;
-                    }
-                }
-                jobUsed[index] = true;
-                positionJob[i] = index + 1;
-            }
-
-            System.out.println("\nPositionJob: ");
-            for(int q = 0; q < positionJob.length; q++){
-                System.out.print(positionJob[q] + ", ");
-            }
-        }
+//            File file = directoryListing[problem];
+//            DataReader reader = new DataReader(file);
+//            int numJobs = reader.numJobs;
+//            int numAgents = reader.numAgents;
+//            int[] processingTimes = reader.processingTimes;
+//            //preferences = int[numAgents][numJobs];
+//            int[][] preferences = reader.preferences;
+//            int[][] preferencesInverse = new int[numAgents][numJobs];
+//
+//            for (int a = 0; a < numAgents; a++) {
+//                for (int j = 0; j < numJobs; j++) {
+//                    preferencesInverse[a][preferences[a][j] - 1] = j + 1;
+//                }
+//            }
+//
+//            System.out.println(numJobs);
+//            System.out.println(numAgents);
+//            System.out.println(Arrays.toString(processingTimes));
+//            System.out.println(Arrays.toString(preferences[0]));
+//            for (int y = 1; y < numAgents; y++) {
+//                System.out.println(Arrays.toString(preferences[y]));
+//            }
+//
+//            //score Array
+//            int[] jobScore = new int[numJobs];
+//
+//            for (int jobk = 0; jobk < numJobs; jobk++) {
+//                for (int jobl = 0; jobl < numJobs; jobl++) {
+//                    int count = 0;
+//                    if (jobk != jobl) {
+//                        for (int agent = 0; agent < numAgents; agent++) {
+//                            if (preferencesInverse[agent][jobk] < preferencesInverse[agent][jobl]) {
+//                                count++;
+//                            }
+//                        }
+//                        if ((float) count >= (((float) processingTimes[jobk] / ((float) processingTimes[jobk] + (float) processingTimes[jobl])) * (float) numAgents)) {
+//                            jobScore[jobk]++;
+//                        }
+//                    }
+//                }
+//            }
+//
+//            System.out.println("Jobscore: ");
+//            for(int q = 0; q < jobScore.length; q++){
+//                System.out.print(jobScore[q] + ", ");
+//            }
+//
+//            // Job on position
+//            int[] positionJob = new int[numJobs];
+//
+//            // Used Job
+//            boolean[] jobUsed = new boolean[numJobs];
+//
+//            for (int i = 0; i < numJobs; i++) {
+//                int score = -1;
+//                int index = -1;
+//                for (int j = 0; j < numJobs; j++) {
+//                    if (score < jobScore[j] && jobUsed[j] == false) {
+//                        score = jobScore[j];
+//                        index = j;
+//                    }
+//                }
+//                jobUsed[index] = true;
+//                positionJob[i] = index + 1;
+//            }
+//
+//            System.out.println("\nPositionJob: ");
+//            for(int q = 0; q < positionJob.length; q++){
+//                System.out.print(positionJob[q] + ", ");
+//            }
+//        }
     }
 }
--- MinizincRunner/src/MiniZincRunner.java
+++ MinizincRunner/src/MiniZincRunner.java
@@ -1,38 +1,75 @@
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MiniZincRunner {
+    int numJobs;
+    int numAgents;
+    int[] processingTimes;
+    //preferences = int[numAgents][numJobs];
+    int[][] preferences;
+    //on jobposition[0] it is specified which job comes first
+    int[] jobPosition;
+    int[][] agentcompletionTimes;
+    int[] completionTimes;
 
-    public static void looper()  throws IOException {
+    public void looper() throws IOException {
         File dir = new File("C:\\Users\\Maarten\\Desktop\\IDM\\gametheory_project\\TestSetGenerator\\TestSets\\RealisticSets");
         File[] directoryListing = dir.listFiles();
         if (directoryListing != null) {
-//            for (int i = 0; i < directoryListing.length; i++) {
-                for (int i = 0; i < 1; i++) {
+            for (int problem = 0; problem < 1; problem++) {
+
+                File file = directoryListing[problem];
+                DataReader reader = new DataReader(file);
+                numJobs = reader.numJobs;
+                numAgents = reader.numAgents;
+                processingTimes = reader.processingTimes;
+                //preferences = int[numAgents][numJobs];
+                preferences = reader.preferences;
+
+//                System.out.println(numJobs);
+//                System.out.println(numAgents);
+//                System.out.println(Arrays.toString(processingTimes));
+//                System.out.println(Arrays.toString(preferences[0]));
+//                for (int y = 1; y < numAgents; y++) {
+//                    System.out.println(Arrays.toString(preferences[y]));
+//                }
 
                 // Do something with child
-                File file = directoryListing[i];
                 long startTime = System.nanoTime();
                 miniZincRunner("minizinc", "C:\\Users\\Maarten\\Desktop\\IDM\\gametheory_project\\model.mzn", file);
                 long endTime = System.nanoTime();
                 long runtime = endTime - startTime;
-                System.out.println("!!! Runtime = " + runtime + "nanoseconds!!!");
+                System.out.println("!!! Runtime = " + runtime/1000000000 + " seconds!!!");
+
             }
         } else {
             System.out.println("directory is empty");
         }
     }
 
-    public static void miniZincRunner(String commd, String model, File dataset)  throws IOException {
-        Process process = new ProcessBuilder(commd, model, dataset+"").start();
+    public void miniZincRunner(String commd, String model, File dataset) throws IOException {
+        Process process = new ProcessBuilder(commd, model, dataset + "").start();
         InputStream is = process.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
         String line;
 
-        System.out.printf("Output of running dataset %s is:", dataset.getName());
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
+        String line1 = br.readLine();
+        String[] schedulestring = line1.substring(line1.indexOf("[") + 1, line1.indexOf("]")).split(", ");
+        this.jobPosition = new int[numJobs];
+        for (int x = 0; x < numJobs; x++) {
+            this.jobPosition[x] = Integer.parseInt(schedulestring[x]);
         }
+        String line2 = br.readLine();
+        String[] completionstring = line2.substring(line2.indexOf("[") + 1, line2.indexOf("]")).split(", ");
+        this.completionTimes = new int[numJobs];
+        for (int x = 0; x < numJobs; x++) {
+            this.completionTimes[x] = Integer.parseInt(completionstring[x]);
+        }
+        System.out.println(Arrays.toString(jobPosition));
+        System.out.println(Arrays.toString(completionTimes));
         return;
     }
 }
